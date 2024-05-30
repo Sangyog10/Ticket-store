94962c9775486f6937f3ddffb751616188c58364
sangyog <sangyog.puri111@gmai.com>
2024-05-30 23:09:21 +0545

    order route handlers and test done

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,33 @@
 import express, { Request, Response } from "express";
+import {
+  UnauthorizedError,
+  notFoundError,
+  requireAuth,
+} from "@santicket/common";
+import { Order } from "../models/order";
+import { OrderStatus } from "@santicket/common";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId).populate("ticket");
+    if (!order) {
+      throw new notFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new UnauthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    //publish an event saying that it was cancelled
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,26 @@
 import express, { Request, Response } from "express";
+import {
+  UnauthorizedError,
+  notFoundError,
+  requireAuth,
+} from "@santicket/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+    if (!order) {
+      throw new notFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new UnauthorizedError();
+    }
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
